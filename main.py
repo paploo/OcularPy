e98d9bf9e16343b41f77483c93aabb05ae902b0f
Jeff Reinecke <jeff@paploo.net>
2021-10-12 19:52:53 -0700

    Dynamic plotting to compare telescopes and plots

--- main.py
+++ main.py
@@ -12,32 +12,38 @@ from ocular.viz import field_stop_diameter_viz
 
 def main():
     eyepiece_lib = csv_loader.load_eyepiece_library(Path('./equipment/eyepieces.csv'))
-    print(eyepiece_lib)
+    telescope_lib = csv_loader.load_telescope_library(Path('./equipment/telescopes.csv'))
+    favorites_lib = csv_loader.load_favorites_library(Path('./equipment/favorites.csv'))
+    all_favs_lib = reduce(lambda a, b: a + b, favorites_lib.catalog.values())
 
-    telescopes = csv_loader.load_telescope_library(Path('./equipment/telescopes.csv'))
-    print(telescopes)
+    telescope = telescope_lib['ORI-XX12G']
+    #telescope = telescope_lib['APR-AD8']
+    #telescope = telescope_lib['AWB-ONESKY']
+    #telescope = telescope_lib['MDE-LX90']
 
-    favorites = csv_loader.load_favorites_library(Path('./equipment/favorites.csv'))
-    print(favorites)
-
-    all_favs = reduce(lambda a, b: a + b, favorites.catalog.values())
-    print(all_favs)
-
-    telescope = telescopes.catalog['ORI-XX12G']
-    #telescope = telescopes.catalog['APR-AD8']
-    #telescope = telescopes.catalog['AWB-ONESKY']
-    #telescope = telescopes.catalog['MDE-LX90']
-
-    eyepieces = eyepiece_lib.filter_favorites(all_favs).catalog.values()
+    eyepieces = eyepiece_lib.filter_favorites(all_favs_lib).catalog.values()
     #eyepieces = eyepiece_lib.catalog.values()
 
-    plt.rcParams['figure.figsize'] = [16, 11]
+    charts = [
+        apparent_field_of_view_viz,
+        field_stop_diameter_viz,
+        true_field_of_view_viz,
+        #true_angle_of_view_viz
+    ]
+    print(charts)
+
+    telescopes = [
+        telescope_lib['ORI-XX12G'],
+        telescope_lib['APR-AD8']
+    ]
+    print(telescopes)
+
+    plt.rcParams['figure.figsize'] = [18, 11]
     plt.rcParams['figure.subplot.hspace'] = 0.40
-    fig, axes = plt.subplots(2, 2)
-    apparent_field_of_view_viz.make_plot(axes[0][0], telescope, eyepieces)
-    field_stop_diameter_viz.make_plot(axes[0][1], telescope, eyepieces)
-    true_field_of_view_viz.make_plot(axes[1][0], telescope, eyepieces)
-    true_angle_of_view_viz.make_plot(axes[1][1], telescope, eyepieces)
+    fig, axes = plt.subplots(len(telescopes), len(charts))
+    for j, telescope in enumerate(telescopes):
+        for i, chart in enumerate(charts):
+            chart.make_plot(axes[j][i], telescope, eyepieces)
     plt.show()
 
     # plt.rcParams['figure.figsize'] = [12,9]
